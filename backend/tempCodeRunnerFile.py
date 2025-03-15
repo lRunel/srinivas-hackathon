6182e9863b4f559cee326dbb91ef2153ca83c5ff
lRunel <shubhamchouta@gmail.com>
2025-03-15 19:54:57 +0530

    first commit everrrrrr

---
+++ backend/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
